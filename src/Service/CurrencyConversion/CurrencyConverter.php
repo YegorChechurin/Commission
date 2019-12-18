8a2f1dc4aa83536fa76bf240835491a8ad13cb84
echechurin <egor.chechurin@simple-solution.biz>
2019-12-18 15:16:01 +0300

    injected CurrencyManager

--- src/Service/CurrencyConversion/CurrencyConverter.php
+++ src/Service/CurrencyConversion/CurrencyConverter.php
@@ -4,43 +4,31 @@ declare(strict_types=1);
 
 namespace YegorChechurin\CommissionTask\Service\CurrencyConversion;
 
-use YegorChechurin\CommissionTask\Service\CurrencyConversion\Exception\UnsupportedCurrencyException;
+use YegorChechurin\CommissionTask\Service\CurrencyManagement\CurrencyManager;
 
 class CurrencyConverter implements CurrencyConverterInterface
 {
-	private const CONVERSION_RATES = [
-		'USD' => 1.1497,
-		'JPY' => 129.53,
-	];
+	/**
+	 * @var CurrencyManager
+	 */
+	private $cm;
+
+	public function __construct(CurrencyManager $cm)
+	{
+		$this->cm = $cm;
+	}
 
 	public function convertFromEuro($currencyName, $eurAmount)
 	{
-		$this->checkCurrencyIsSupported($currencyName);
+		$rate = $this->cm->getCurrencyConversionRate($currencyName);
 
-		return $eurAmount * $this->getConversionRate($currencyName);
+		return $eurAmount * $rate;
 	}
 
 	public function convertToEuro($currencyName, $currencyAmount)
 	{
-		$this->checkCurrencyIsSupported($currencyName);
+		$rate = 1 / $this->cm->getCurrencyConversionRate($currencyName);
 		
-		return $currencyAmount * $this->getReverseConversionRate($currencyName);
-	}
-
-	private function getConversionRate(string $currencyName)
-	{
-		return self::CONVERSION_RATES[$currencyName];
-	}
-
-	private function getReverseConversionRate(string $currencyName)
-	{
-		return 1 / $this->getConversionRate($currencyName);
-	}
-
-	private function checkCurrencyIsSupported(string $currencyName): void
-	{
-		if (!in_array($currencyName, array_keys(self::CONVERSION_RATES))) {
-			throw UnsupportedCurrencyException($currencyName);
-		}
+		return $currencyAmount * $rate;
 	}
 }

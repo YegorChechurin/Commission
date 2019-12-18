467905c61109577d03539a68090acc403a2c05ee
echechurin <egor.chechurin@simple-solution.biz>
2019-12-18 15:12:09 +0300

    added methods

--- src/Service/CurrencyManagement/CurrencyManager.php
+++ src/Service/CurrencyManagement/CurrencyManager.php
@@ -2,17 +2,40 @@
 
 namespace YegorChechurin\CommissionTask\Service\CurrencyManagement;
 
+use Symfony\Component\Yaml\Yaml;
+use YegorChechurin\CommissionTask\Service\CurrencyManagement\Exception\RuntimeException\UnsupportedCurrencyException;
+
 class CurrencyManager
 {
-	private $currencyConfig;
+	private const NUM_OF_DIR_TO_GO_UP_TO_CONFIG = 3;
+
+	private const CONF_FILE_LOCATION = '/config/currencies.yaml';
+
+	private $currencies;
 
 	public function __construct()
 	{
-		$this->currencyConfig = \yaml_parse_file(dirname(__DIR__, 3).'/config/currencies.yaml');
+		$this->currencies = Yaml::parseFile(dirname(__DIR__, self::NUM_OF_DIR_TO_GO_UP_TO_CONFIG).self::CONF_FILE_LOCATION);
 	}
 
-	public function getConfig()
+	public function getCurrencyConversionRate(string $currencyName)
 	{
-		return $this->currencyConfig;
+		$this->checkCurrencyIsSupported($currencyName);
+
+		return $this->currencies[$currencyName]['conversion_rate'];
+	}
+
+	public function getNumberOfDecimalDigitsOfCurrencySmallestItem(string $currencyName)
+	{
+		$this->checkCurrencyIsSupported($currencyName);
+		
+		return $this->currencies[$currencyName]['number_of_decimal_digits_of_smallest_item'];
+	}
+
+	public function checkCurrencyIsSupported(string $currencyName)
+	{
+		if (!in_array($currencyName, array_keys($this->currencies))) {
+			throw new UnsupportedCurrencyException($currencyName);
+		}
 	}
 }

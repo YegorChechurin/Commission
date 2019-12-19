09685a8515ae35adaddc053b9efdfb30be5c4cec
echechurin <egor.chechurin@simple-solution.biz>
2019-12-19 19:48:22 +0300

    added methods checkNumberIsDecimal and splitDecimalIntoWholeAndFractional

--- src/Service/Math/Math.php
+++ src/Service/Math/Math.php
@@ -1,12 +1,12 @@
 <?php
 
-declare(strict_types=1);
+//declare(strict_types=1);
 
 namespace YegorChechurin\CommissionTask\Service\Math;
 
 class Math
 {
-    private $scale;
+    /*private $scale;
 
     public function __construct(int $scale)
     {
@@ -21,5 +21,30 @@ class Math
     public function multiply(string $leftOperand, string $rightOperand): string
     {
         return bcmul($leftOperand, $rightOperand, $this->scale);
+    }*/
+
+    public function checkNumberIsDecimal($number): bool
+    {
+        if (1 === preg_match('%\.%', $number)) {
+            return true;
+        } elseif ((int)0 === preg_match('%\.%', $number)) {
+            return false;
+        } else {
+            throw new CheckNumberIsDecimalException((string) $number);
+        }
+    }
+
+    public function splitDecimalIntoWholeAndFractional($decimal): array
+    {
+        if ($this->checkNumberIsDecimal($decimal)) {
+            $parts = explode('.', strval($decimal));
+
+            return [
+                'whole' => $parts[0],
+                'fractional' => $parts[1],
+            ];
+        } else {
+            throw new NumberIsNotDecimalException((string) $decimal);
+        }
     }
 }

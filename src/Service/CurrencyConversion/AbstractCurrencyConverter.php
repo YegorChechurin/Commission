35a38b4d283a1667d59e8e6ffec685720bb0c849
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-22 19:07:06 +0300

    created AbstractCurrencyConverter and moved constructor of CurrencyConverter there

---
+++ src/Service/CurrencyConversion/AbstractCurrencyConverter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace YegorChechurin\CommissionTask\Service\CurrencyConversion;
+
+use YegorChechurin\CommissionTask\Service\DomainLogicSettings\CurrencyManagement\CurrenciesManager;
+
+abstract class AbstractCurrencyConverter implements CurrencyConverterInterface
+{
+	/**
+	 * @var CurrenciesManager
+	 */
+	protected $cm;
+
+	abstract public function convertFromEuro(string $currencyName, $amountInEUR);
+
+	abstract public function convertToEuro(string $currencyName, $amountInCurrencyToBeConverted);
+
+	public function __construct(CurrenciesManager $cm)
+	{
+		$this->cm = $cm;
+	}
+}

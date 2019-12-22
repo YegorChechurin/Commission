49d01884c84de5c0bb132e4611f0aedc7816a6ef
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-22 19:04:42 +0300

    aded typehint for $currencyName parameter into method signature of CurrencyConverterInterface methods

--- src/Service/CurrencyConversion/CurrencyConverterInterface.php
+++ src/Service/CurrencyConversion/CurrencyConverterInterface.php
@@ -4,7 +4,7 @@ namespace YegorChechurin\CommissionTask\Service\CurrencyConversion;
 
 interface CurrencyConverterInterface
 {
-	public function convertFromEuro($currencyName, $amountInEUR);
+	public function convertFromEuro(string $currencyName, $amountInEUR);
 	
-	public function convertToEuro($currencyName, $amountInCurrencyToBeConverted);
+	public function convertToEuro(string $currencyName, $amountInCurrencyToBeConverted);
 }

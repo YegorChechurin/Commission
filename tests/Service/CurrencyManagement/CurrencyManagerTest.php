e88097bf3ad08553fb4bf40e9da59f941aaf29ea
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-22 19:11:36 +0300

    modified CurrencyManagerTest

--- tests/Service/CurrencyManagement/CurrencyManagerTest.php
+++ tests/Service/CurrencyManagement/CurrencyManagerTest.php
@@ -3,9 +3,10 @@
 namespace YegorChechurin\CommissionTask\Tests\Service\CurrencyManagement;
 
 use PHPUnit\Framework\TestCase;
-use YegorChechurin\CommissionTask\Service\CurrencyManagement\CurrencyManager;
-use YegorChechurin\CommissionTask\Service\CurrencyManagement\Exception\RuntimeException\UnsupportedCurrencyException;
-use YegorChechurin\CommissionTask\Service\DomainLogicSettings\CurrenciesManager as CM;
+/*use YegorChechurin\CommissionTask\Service\CurrencyManagement\CurrencyManager;
+use YegorChechurin\CommissionTask\Service\CurrencyManagement\Exception\RuntimeException\UnsupportedCurrencyException;*/
+use YegorChechurin\CommissionTask\Service\FileParsing\FileParserFactory;
+use YegorChechurin\CommissionTask\Service\DomainLogicSettings\CurrencyManagement\CurrenciesManager as CM;
 
 class CurrencyManagerTest extends TestCase
 {
@@ -16,15 +17,15 @@ class CurrencyManagerTest extends TestCase
 
 	public function setUp()
 	{
-		$this->manager = new CurrencyManager();
+		//$this->manager = new CurrencyManager();
 	}
 
 	public function testGetConfig()
 	{
 		//$this->expectException(UnsupportedCurrencyException::class);
 		//var_dump($this->manager->getNumberOfDecimalDigitsOfCurrencySmallestItem('USA'));
-		$cm = new CM();
-		var_dump($cm->test());
+		$cm = new CM(new FileParserFactory());
+		var_dump($cm->getCurrencyConversionRate('EU'));
 
 		$this->assertTrue(true);
 	}

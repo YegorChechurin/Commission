5f1c25e77a97e12f2c12f30b8d16ef2672e98494
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-22 14:15:43 +0300

    created NoSettingsFileException

---
+++ src/Service/DomainLogicSettings/Exception/LogicException/NoSettingsFileException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace YegorChechurin\CommissionTask\Service\DomainLogicSettings\Exception\LogicException;
+
+class NoSettingsFileException extends \LogicException
+{
+	public function __construct(
+		string $settingsFileName, 
+		string $settingFilesFolderPath
+	)
+	{
+		parent::__construct(
+			sprintf('No settings file found, you have to create %s file in %s', $settingsFileName, $settingFilesFolderPath)
+		);
+	}
+}

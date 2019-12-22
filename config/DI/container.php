938d5e9467c73c4faf529168893322811a5aa301
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-22 18:59:20 +0300

    added FileParserFactoryInterface definition to DI container config

--- config/DI/container.php
+++ config/DI/container.php
@@ -1,8 +1,13 @@
 <?php
 
+use YegorChechurin\CommissionTask\Service\FileParsing\FileParserFactoryInterface;
+use YegorChechurin\CommissionTask\Service\FileParsing\FileParserFactory;
 use YegorChechurin\CommissionTask\Service\CurrencyConversion\CurrencyConverterInterface;
 use YegorChechurin\CommissionTask\Service\CurrencyConversion\CurrencyConverter;
 
+use function DI\get;
+
 return [
-    CurrencyConverterInterface::class => DI\get(CurrencyConverter::class),
+	FileParserFactoryInterface::class => get(FileParserFactory::class),
+    CurrencyConverterInterface::class => get(CurrencyConverter::class),
 ];

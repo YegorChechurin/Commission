1af8ec1b6a91b0f09845b0fb053eb6ff29008b99
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-22 19:10:45 +0300

    made CurrencyConverter final and made it extend AbstractCurrencyConverter

--- src/Service/CurrencyConversion/CurrencyConverter.php
+++ src/Service/CurrencyConversion/CurrencyConverter.php
@@ -4,28 +4,16 @@ declare(strict_types=1);
 
 namespace YegorChechurin\CommissionTask\Service\CurrencyConversion;
 
-use YegorChechurin\CommissionTask\Service\CurrencyManagement\CurrencyManager;
-
-class CurrencyConverter implements CurrencyConverterInterface
+final class CurrencyConverter extends AbstractCurrencyConverter
 {
-	/**
-	 * @var CurrencyManager
-	 */
-	private $cm;
-
-	public function __construct(CurrencyManager $cm)
-	{
-		$this->cm = $cm;
-	}
-
-	public function convertFromEuro($currencyName, $eurAmount)
+	public function convertFromEuro(string $currencyName, $eurAmount)
 	{
 		$rate = $this->cm->getCurrencyConversionRate($currencyName);
 
 		return $eurAmount * $rate;
 	}
 
-	public function convertToEuro($currencyName, $currencyAmount)
+	public function convertToEuro(string $currencyName, $currencyAmount)
 	{
 		$rate = 1 / $this->cm->getCurrencyConversionRate($currencyName);
 		

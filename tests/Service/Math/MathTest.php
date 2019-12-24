da9f8e0bfced0ef763e17f70ddf8a1a26ecb3db9
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-25 00:01:28 +0300

    added new tests to MathTest in order to test roundSpecificDigitAfterPointToUpperBound

--- tests/Service/Math/MathTest.php
+++ tests/Service/Math/MathTest.php
@@ -7,6 +7,7 @@ namespace YegorChechurin\CommissionTask\Tests\Service;
 use PHPUnit\Framework\TestCase;
 use YegorChechurin\CommissionTask\Service\Math\Math;
 use YegorChechurin\CommissionTask\Service\Math\Exception\LogicException\NumberIsNotDecimalException;
+use YegorChechurin\CommissionTask\Service\Math\Exception\LogicException\InvalidPostionAfterPointException;
 
 class MathTest extends TestCase
 {
@@ -28,13 +29,6 @@ class MathTest extends TestCase
         $this->assertTrue($this->math->checkNumberIsDecimal($number));
     }
 
-    public function decimalNumberProvider()
-    {
-        return [
-            ['25.456'], ['8.17']
-        ];
-    }
-
     /**
      * @dataProvider integerNumberProvider
      */
@@ -53,10 +47,47 @@ class MathTest extends TestCase
         $this->math->splitDecimalIntoWholeAndFractional($number);
     }
 
+    public function decimalNumberProvider()
+    {
+        return [
+            ['25.456'], ['8.17']
+        ];
+    }
+
     public function integerNumberProvider()
     {
         return [
             ['8'], ['777']
+        ];
+    }
+
+    public function testExceptionalRoundSpecificDigitAfterPointToUpperBound()
+    {
+        $this->expectException(InvalidPostionAfterPointException::class);
+
+        $this->math->roundSpecificDigitAfterPointToUpperBound('12.13', -5);
+    }
+
+    /** 
+     * @dataProvider roundSpecificDigitAfterPointToUpperBoundProvider 
+     */
+    public function testRoundSpecificDigitAfterPointToUpperBound(string $decimal, int $positionAfterPoint, string $expectation)
+    {
+        $this->assertEquals(
+            $expectation,
+            $this->math->roundSpecificDigitAfterPointToUpperBound(
+                $decimal, $positionAfterPoint
+            )
+        );
+    }
+
+    public function roundSpecificDigitAfterPointToUpperBoundProvider()
+    {
+        return [
+            ['12.131', 2, '12.14'],
+            ['12.00001', 4, '12.0001'],
+            ['12.00', 0, '12'],
+            ['12.0000000001', 0, '13']
         ];
     }
 
